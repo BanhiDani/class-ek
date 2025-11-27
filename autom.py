baac5ec2c64babba0b68bd75021eb8fca7f50b0a
BanhiDani <sistbroo@gmail.com>
2025-11-27 14:57:48 +0100

    minden keszen

--- autom.py
+++ autom.py
@@ -15,8 +15,34 @@ def file_beolvas():
         auto=Auto(sor[0],int(sor[1]))
         i += 1
         auto_lista.append(auto)
-    print( auto_lista[0].gyartasi_ev)
+    """print( auto_lista[0].gyartasi_ev)
     print(auto_lista[1])
-    print(auto_lista[0])
+    print(auto_lista[0])"""
+    return auto_lista
 
 file_beolvas()
+
+def flotta(auto_lista):
+    autok_szama = len(auto_lista)
+    print (f"Autók száma: {autok_szama}")
+
+def legfiatalabb(auto_lista):
+    i = 0
+    fiatal = 0
+    melyik = ""
+    while i < len(auto_lista):
+        if auto_lista[i].gyartasi_ev > fiatal:
+            fiatal = auto_lista[i].gyartasi_ev
+            melyik =auto_lista[i].nev
+        i += 1
+    print (f"A legfiatalabb autó: {melyik}({fiatal})")
+
+def atlagolas(auto_lista):
+    i = 0
+    valtozo = 0
+    while i < len(auto_lista):
+        valtozo +=  auto_lista[i].gyartasi_ev
+        i += 1
+    atlag = valtozo / len(auto_lista)
+    eredmeny = int(2025 - atlag)
+    print (f"Az autók átlagos kora: {eredmeny} év")
--- main.py
+++ main.py
@@ -1,6 +1,19 @@
+import bevezetes
 import sorozat
+import autom
 
+print ("****** 1. Feladat ******")
+bevezetes.auto_allapota()
+
+print ("****** 2. Feladat ******")
 szamok = sorozat.lotto()
 egyjegyuek = sorozat.egyjegyuek_szama(szamok)
 sorozat.konzol_kiir(egyjegyuek)
 sorozat.file_kiir(egyjegyuek)
+
+print("****** 3. Feladat ******")
+
+auto_lista = autom.file_beolvas()
+autom.flotta(auto_lista)
+autom.legfiatalabb(auto_lista)
+autom.atlagolas(auto_lista)
